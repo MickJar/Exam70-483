f1d7d61771ce51e5353369c6108f43289185f987
Mikael Jarfors <nat13mja@student.lu.se>
2018-12-11 22:10:00 +0100

    Initial taskRace commit

---
+++ TaskRace/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Red;
+            this.progressBar1.Location = new System.Drawing.Point(12, 43);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(976, 73);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.ForeColor = System.Drawing.Color.Green;
+            this.progressBar2.Location = new System.Drawing.Point(12, 138);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(976, 73);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.ForeColor = System.Drawing.Color.Blue;
+            this.progressBar3.Location = new System.Drawing.Point(12, 230);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(976, 73);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.ForeColor = System.Drawing.Color.Black;
+            this.progressBar4.Location = new System.Drawing.Point(12, 329);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(976, 73);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 701);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(450, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(538, 701);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(450, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 800);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TaskRace/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.button1.Click += new EventHandler(button1_Click);
+            button1.Text = "Start WaitAny Race";
+            this.button2.Click += new EventHandler(button2_Click);
+            button2.Text = "Start WaitAll Race";
+        }
+
+        public static int DoWork(Form1 form, int no, Random rng, CancellationToken ct)
+        {
+            int count = 0;
+            while (count < 20 & !ct.IsCancellationRequested) {
+                Thread.Sleep(rng.Next(0, 2000));
+                form.incrementProgressBar(no);
+
+                count++;
+            }
+            return no;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            resetProgressBars();
+            int noThreads = 4;
+            Random rng = new Random();
+            Task<int>[] tasks = new Task<int>[noThreads];
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+
+            for (int i = 0; i < noThreads; i++)
+            {
+                int currentIndex = i + 1; // Make a local copy of the loop counter so that the
+                                          // correct task number is passed into the lambda function
+                tasks[i] = new Task<int>(() => DoWork(this, currentIndex, rng, token));
+            }
+
+            foreach (Task task in tasks)
+            {
+                task.Start();
+            }
+            //foreach(Task task in tasks)
+            //{
+            //    task.Wait();
+            //}
+            var winner = await Task.WhenAny(tasks);
+            tokenSource.Cancel();
+            MessageBox.Show("The Winner is: " + winner.Result, "Congratulations", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            resetProgressBars();
+            int noThreads = 4;
+            Random rng = new Random();
+            Task<int>[] tasks = new Task<int>[noThreads];
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+
+            for (int i = 0; i < noThreads; i++)
+            {
+                int currentIndex = i + 1; // Make a local copy of the loop counter so that the
+                                          // correct task number is passed into the lambda function
+                tasks[i] = new Task<int>(() => DoWork(this, currentIndex, rng, token));
+            }
+
+            foreach (Task task in tasks)
+            {
+                task.Start();
+            }
+            //foreach(Task task in tasks)
+            //{
+            //    task.Wait();
+            //}
+            var winner = await Task.WhenAll(tasks);
+            tokenSource.Cancel();
+            MessageBox.Show("The Winner is: " + "Everyone is a winner", "Congratulations", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+        private void resetProgressBars()
+        {
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+            progressBar4.Value = 0;
+        }
+        public void incrementProgressBar(int identifier)
+        {
+            ProgressBar progBar = null;
+            switch (identifier)
+            {
+                case 1:
+                    progBar = progressBar1;
+                    break;
+                case 2:
+                    progBar = progressBar2;
+                    break;
+                case 3:
+                    progBar = progressBar3;
+                    break;
+                case 4:
+                    progBar = progressBar4;
+                    break;
+            }
+
+            if (this.InvokeRequired)
+            {
+                this.EndInvoke(this.BeginInvoke(new MethodInvoker(delegate () { progBar.Value+=5; })));
+            }
+            else
+            {
+                progBar.Value+=5;
+            }
+        }
+    }
+}
